573fef912f3e0c115602f8b97447decc4b5932c2
nusrat bornona <pagli@macbook-air.lan>
2021-04-06 19:57:00 -0400

    added selenium project

---
+++ Maven_BTC/src/main/java/util/Source1.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Source1 {
+	
+	public void m1() {
+		System.out.println("hi");
+	}
+
+}
---
+++ Maven_BTC/src/test/java/com/SmokeTest.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.Test;
+
+import util.Source1;
+
+public class SmokeTest {
+	
+	@Test
+	public void test1() {
+		Source1 s=new Source1();
+		s.m1();
+	}
+
+}
